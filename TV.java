05c5a21ca42018b662b4239842a9563093e63c50
1Esterandrade <20251ti0052@ifba.edu.br>
2026-03-03 09:10:37 -0300

    volume ok

--- TV.java
+++ TV.java
@@ -1,1 +1,34 @@
+class TV { 
+    int tamanhoTela = 98;
+    int consumo;
+    int volume=5;
+    string marca;
+    string modelo;
+    int voltagem= 220;
+    int canal;
+    int ano;    
+
+void ligar (){
+  
+    consumo=tamanhoTela*voltagem;
+    printf("Consumo é de: %d", consumo);
+}
+
+void desligar (){
+
+}
+
+int aumentarVolume (){
+  if (volume<10){
+    volume++;
+  }
+  return volume; 
+}
+
+int abaixarVolume (){
+  if (volume>10 ){
+    volume--;
+  }
+  return volume; 
+}
 
