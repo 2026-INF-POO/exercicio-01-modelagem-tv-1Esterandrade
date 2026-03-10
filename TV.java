8e66e812559930aac0882cbc4c6da404ffdb7a41
1Esterandrade <20251ti0052@ifba.edu.br>
2026-03-09 21:20:55 -0300

    Atualizar o TV.java

--- TV.java
+++ TV.java
@@ -1,34 +1,55 @@
-class TV { 
-    int tamanhoTela = 98;
-    int consumo;
-    int volume=5;
-    string marca;
-    string modelo;
-    int voltagem= 220;
+class Tv {
+    
+    int tamanhoTela; 
+    int volume = 5;
+    String marca;
+    int voltagem;
     int canal;
-    int ano;    
+    boolean ligada = false;  
 
-void ligar (){
-  
-    consumo=tamanhoTela*voltagem;
-    printf("Consumo é de: %d", consumo);
-}
+    public Tv(int tamanho, String marca, int voltagem) {
+         this.tamanhoTela = tamanho;
+         this.marca = marca;
+         this.voltagem = voltagem; 
+         this.canal = 1; 
+    }
+ 
+    public void ligar() { 
+         this.ligada = true;
+         int consumo = this.voltagem * this.tamanhoTela;
+         System.out.println("Tv Ligada. Consumo: " + consumo); 
+    } 
 
-void desligar (){
+    public void desligar() { 
+         this.ligada = false; 
+         System.out.println("Tv Desligada");
+    }
 
-}
+    public void aumentarVolume() { 
+        if (ligada && volume < 10) {
+            volume++; 
+            System.out.println("Volume: " + volume); 
+        } 
+    } 
 
-int aumentarVolume (){
-  if (volume<10){
-    volume++;
-  }
-  return volume; 
-}
+    public void abaixarVolume() { 
+        if (ligada && volume > 1) {
+            volume--; 
+            System.out.println("Volume: " + volume);
+        }
+    }
 
-int abaixarVolume (){
-  if (volume>10 ){
-    volume--;
-  }
-  return volume; 
-}
+    public void subirCanal() { 
+        if (ligada) { 
+            canal++; 
+            System.out.println("Canal: " + canal); 
+        } 
+    } 
 
+    public void descerCanal() {
+        if (ligada && canal > 1) { 
+            canal--; 
+            System.out.println("Canal: " + canal); 
+        }
+    }
+} 
